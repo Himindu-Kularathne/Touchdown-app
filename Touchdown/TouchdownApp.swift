47fe7c79f34373b4635451fb9a52208ed163c519
Himindu <himindukularathne@gmail.com>
2024-01-03 20:33:16 +0530

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Himindu Kularathne on 2024-01-03.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
